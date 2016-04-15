25ade43f422e27d7e36f4e38f3f3536e76324943
Sarah Lyne <lynesarah@rocketmail.com>
2016-04-15 16:25:17 +1200

    completed project

--- js/api.js
+++ js/api.js
@@ -1,44 +1,69 @@
-
-
 $(document).ready(function(){
 
-	function getVids(){
+	var input;
+	var uploads = "UU8SnqExzYgo_rbZmxwvupww";
+	var nightFlights = "PLX4zKNveXF_w9yYCUzOfnz99rhDmOxKRG";
+	
+
+	$(".nightFlights").on('click', function(){
+					$('#results').empty();
+					  input = nightFlights;
+					  console.log(input);
+					  getVids(input);
+
+				});
+
+	$(".allUploads").on('click', function(){
+					$('#results').empty();
+					input = uploads;
+					console.log(input);
+					getVids(input);
+				});
+
+
+
+	function getVids(input){
 		$.get(
 		"https://www.googleapis.com/youtube/v3/playlistItems",{
 			part: 'snippet',
 			maxResults: 10,
-			order: 'date',
-			playlistId: "UU8SnqExzYgo_rbZmxwvupww",
+			// order: 'date',
+			playlistId: input,
 			key: 'AIzaSyBJax5posLI_66Sfy1RMnVE3_oT1xYg6gI'},
 
 			function(data) {
 				var output;
 				$.each(data.items, function(i, item){
 					
-					videoTitle = item.snippet.title + '<br/>';
+					videoTitle = '<h3>' + item.snippet.title + '</h3>';
 					videoId = item.snippet.resourceId.videoId ;
-					description = item.snippet.description;
-					output = '<li><iframe src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
+					description = '<p>' + item.snippet.description + '</p>';
+					output = '<div class= "api-video"><iframe src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></div>';
+					videoComplete = ' <div class=\"api-video-full\"> ' + videoTitle + description + output + '</div>';
 
-					$('#results').append(videoTitle, description, output, '<hr />');
+					
+				
+					
+				
+					$('#results').append(videoComplete);
+			
 
 				})
 
 			}
+
 		);
+
+			
+
+			
+		
 
 
 	}
 
 
-// $('#nightFlights').click(function(){
-// 						$('nfVideos:hidden').show();
-// 						$('nfVideos:visible').hide();
 
-// 					})
-
-
-getVids();
 
 
 
